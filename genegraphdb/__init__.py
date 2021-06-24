b316e5a5ba671e37d34f88ee8b14ce04044e3826
Matthew Durrant <mdurrant@c02f27qjml7h.lan>
2021-06-23 19:44:42 -0600

    working on loading the proteins

---
+++ genegraphdb/__init__.py
@@ -0,0 +1,6 @@
+DBNAME = "genegraphdb"
+DBURI = "bolt://localhost:7687"
+DBUSER = "neo4j"
+DBPASSWORD = "genegraphdb"
+
+NEO4J_ADMIN = "/Users/mdurrant/scripts/neo4j-community-4.3.1/bin/neo4j-admin"
--- genegraphdb/main.py
+++ genegraphdb/main.py
@@ -1,5 +1,7 @@
 import click
-
+from genegraphdb import *
+from genegraphdb import graphdb
+from genegraphdb import _load
 
 @click.group()
 def cli():
@@ -7,27 +9,39 @@ def cli():
     pass
 
 @cli.command(short_help='Create a new GeneGraphDB')
-@click.argument('db')
-def createdb(db):
-    pass
+def createdb():
+
+    if not graphdb.hasdb():
+        graphdb.createdb()
+    else:
+        print("Database %s already exists." % DBNAME)
+
+@cli.command(short_help='Create a new GeneGraphDB')
+def dbinfo():
+
+    if not graphdb.hasdb():
+        graphdb.createdb()
+
+    print("Total nodes in database:", graphdb.num_nodes())
+    print("Total relationships in database:", graphdb.num_rels())
+
 
 @cli.group(short_help='Load data into the graph database.')
 def load():
     pass
 
 @load.command(short_help='Load a single sample into the database.')
-@click.argument('db')
+@click.option('--sample_id', '-s', required=True, help='The nucleotide FASTA file of the genome.')
 @click.option('--fasta', '-f', required=True, help='The nucleotide FASTA file of the genome.', type=click.Path(exists=True))
 @click.option('--protein', '-p', required=True, help='The protein FASTA file.', type=click.Path(exists=True))
-@click.option('--gff', '-p', required=True, help='The GFF file.', type=click.Path(exists=True))
+@click.option('--gff', '-g', required=True, help='The GFF file.', type=click.Path(exists=True))
+@click.option('--contigs', '-g', required=True, help='The contigs file.', type=click.Path(exists=True))
 @click.option('--google-bucket', '-gb', default=None, help='The Google bucket to store sequences.')
-def single(db, fasta, protein, gff, google_bucket):
-    print(db, fasta, protein, gff)
-    pass
+def single(sample_id, fasta, protein, gff, contigs, google_bucket):
+    _load._single(sample_id, fasta, protein, gff, contigs, google_bucket)
 
 @load.command(short_help='Load multiple samples into the database.')
-@click.argument('db')
-def multi(db):
+def multi():
     pass
 
 @cli.command(short_help='Ådd clusters to protein nodes in the database.')
