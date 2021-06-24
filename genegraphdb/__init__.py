95909a2f7c607441b9c50f1ea16a23b2e4602adf
Matthew Durrant <mdurrant@c02f27qjml7h.lan>
2021-06-23 22:42:14 -0600

    finished loading proteins, contigs, and gene coords

--- genegraphdb/__init__.py
+++ genegraphdb/__init__.py
@@ -2,5 +2,3 @@ DBNAME = "genegraphdb"
 DBURI = "bolt://localhost:7687"
 DBUSER = "neo4j"
 DBPASSWORD = "genegraphdb"
-
-NEO4J_ADMIN = "/Users/mdurrant/scripts/neo4j-community-4.3.1/bin/neo4j-admin"
--- genegraphdb/_load.py
+++ genegraphdb/_load.py
@@ -4,17 +4,23 @@ import gzip
 import re
 import sys
 import os
+from os import remove
 from genegraphdb import graphdb
+from os.path import abspath
+import time
 
 def _single(sample_id, fasta, protein, gff, contigs, google_bucket):
 
     load_proteins(protein)
+    recid2contig = load_fasta(fasta, contigs)
+    load_gene_coords(gff, recid2contig)
 
 
 def load_proteins(protein):
-
+    print("Loading proteins...")
+    tic = time.time()
     outfile = open("proteins.tmp.csv", "w")
-    print("ProteinId:ID,length:int,:LABEL", file=outfile)
+    print("hashid,length", file=outfile)
 
     done = set()
     handle = gzip.open(protein, 'rt')
@@ -24,24 +30,98 @@ def load_proteins(protein):
                 continue
         done.add(name_truncate)
         length = len(str(rec.seq.strip('*')))
-        print(name_truncate+","+str(length)+",Protein", file=outfile)
+        print(name_truncate+","+str(length), file=outfile)
     outfile.close()
+    del done
 
-    cmd = "{neo4j_admin} import --database {db} --nodes {proteins}".format(
-        neo4j_admin=NEO4J_ADMIN, db=DBNAME, proteins="proteins.tmp.csv"
+    conn = graphdb.Neo4jConnection(DBURI, DBUSER, DBPASSWORD)
+    cmd = "LOAD CSV WITH HEADERS FROM 'file:///{csv}' AS row MERGE (n:Protein {{hashid: row.hashid, length:row.length}})".format(
+        csv=abspath('proteins.tmp.csv')
     )
-    os.system(cmd)
+    print(cmd)
+    conn.query(cmd, db=DBNAME)
+    conn.close()
+    toc = time.time()
+    remove('proteins.tmp.csv')
+    print("Loading proteins took %f seconds" % (toc-tic))
+
+
 
 def load_fasta(fasta, contigs):
-    conn = graphdb.Neo4jConnection(DBURI, DBUSER, DBPASSWORD)
+    print("Loading contigs...")
+    tic = time.time()
 
+    recid2contig = dict()
     with gzip.open(contigs, 'rt') as infile:
-        pass
+        infile.readline()
+        for line in infile:
+            line = line.strip().split('\t')
+            recid2contig[line[0]] = (line[1], int(line[3]))
 
-    handle = gzip.open(protein, 'rt')
+    outfile = open("contigs.tmp.csv", "w")
+    print("hashid,length", file=outfile)
+
+    done = set()
+    handle = gzip.open(fasta, 'rt')
     for rec in SeqIO.parse(handle, 'fasta'):
-        name_truncate = rec.id[:20]
-        graphdb.add_node("Protein", {"hashid": name_truncate}, conn)
+        contig, clength = recid2contig[rec.id]
+        name_truncate = contig[:20]
+        if name_truncate in done:
+                continue
+        done.add(name_truncate)
+        length = len(str(rec.seq.strip('*')))
+        print(name_truncate+","+str(clength), file=outfile)
+    outfile.close()
+    del done
 
-    handle.close()
+    conn = graphdb.Neo4jConnection(DBURI, DBUSER, DBPASSWORD)
+    cmd = "LOAD CSV WITH HEADERS FROM 'file:///{csv}' AS row MERGE (n:Contig {{hashid: row.hashid, length:row.length}})".format(
+        csv=abspath('contigs.tmp.csv')
+    )
+    print(cmd)
+    conn.query(cmd, db=DBNAME)
+    conn.close()
+    toc = time.time()
 
+    print("Loading contigs took %f seconds" % (toc-tic))
+    remove('contigs.tmp.csv')
+
+    return recid2contig
+
+def load_gene_coords(gff, recid2contig):
+    print("Loading gene coords...")
+    tic = time.time()
+    outfile = open("gene_coords.tmp.csv", "w")
+    print("phash,chash,start,end,orient", file=outfile)
+    with gzip.open(gff, "rt") as infile:
+        for line in infile:
+            if line.startswith("#"):
+                continue
+            line = line.strip().split('\t')
+            if len(line) != 9:
+                print(len(line))
+                continue
+
+            phash = line[-1].split("ID=")[-1].split(';')[0][:20]
+            chash = recid2contig[line[0]][0][:20]
+
+            print(phash, chash, line[3], line[4], line[6], sep=',', file=outfile)
+    outfile.close()
+
+    conn = graphdb.Neo4jConnection(DBURI, DBUSER, DBPASSWORD)
+    cmd = """
+          LOAD CSV WITH HEADERS FROM 'file:///{csv}' AS row 
+          MATCH (p:Protein), (c:Contig) 
+          WHERE p.hashid = row.phash AND c.hashid = row.chash 
+          MERGE (p)-[r:GeneCoord {{start: row.start, end: row.end, orient: row.orient}}]->(c)
+          """.format(
+        csv=abspath('gene_coords.tmp.csv')
+    )
+    print(cmd)
+    conn.query(cmd, db=DBNAME)
+    conn.close()
+    toc = time.time()
+
+    print("Loading gene coords took %f seconds" % (toc-tic))
+
+    #remove("gene_coords.tmp.csv")
