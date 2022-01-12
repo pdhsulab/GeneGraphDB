385251357680c8b5dd3c7bcf0b4683a3cf577753
Matt Durrant <durrantmm@gmail.com>
2022-01-12 16:39:37 -0700

    update

--- GGDBFetch/ggdbfetch/sample2protein.py
+++ GGDBFetch/ggdbfetch/sample2protein.py
@@ -5,9 +5,9 @@ from collections import defaultdict
 from multiprocessing import Pool
 
 def get_sample_to_p100s(p100s, dbpath, sample2path, threads):
-    con = sqlite3.connect(join(dbpath, SAMPLE2PROTEIN_DB))
+    dbpath = join(dbpath, SAMPLE2PROTEIN_DB)
 
-    args = [(p100, con, sample2path) for p100 in p100s]
+    args = [(p100, dbpath, sample2path) for p100 in p100s]
     with Pool(threads) as pool:
         results = pool.starmap(get_sample_id, args)
     sample2p100s = defaultdict(set)
@@ -15,11 +15,11 @@ def get_sample_to_p100s(p100s, dbpath, sample2path, threads):
         for samp in res:
             for p100 in res[samp]:
                 sample2p100s[samp].add(p100)
-    con.close()
 
     return dict(sample2p100s)
 
-def get_sample_id(p100, con, sample2path):
+def get_sample_id(p100, dbpath, sample2path):
+    con = sqlite3.connect(dbpath)
     cur = con.cursor()
     cmd = 'SELECT sample_id FROM sample2protein WHERE p100="{}"'.format(p100)
     sample2p100s = defaultdict(set)
@@ -28,4 +28,5 @@ def get_sample_id(p100, con, sample2path):
         if samp in sample2path:
             sample2p100s[res[0]].add(p100)
     cur.close()
+    con.close()
     return sample2p100s
