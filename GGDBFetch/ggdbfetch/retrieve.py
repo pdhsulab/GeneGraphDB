d3656660b5ecbd3148c75e64d4ed823dbc86c69d
Matt Durrant <durrantmm@gmail.com>
2022-01-12 16:03:33 -0700

    update

--- GGDBFetch/ggdbfetch/retrieve.py
+++ GGDBFetch/ggdbfetch/retrieve.py
@@ -22,6 +22,7 @@ def retrieve_target_and_bait(target_id, bait_ids, dbpath, sample2path):
     all_p100, p100_to_p90, p100_to_p30 = clusters.get_clusters(target_id, dbpath)
     sample2p100s = sample2protein.get_sample_to_p100s(all_p100, dbpath, sample2path)
 
+    print(sample2p100s)
     for samp in sample2p100s:
         contigs, gene_coords = regions.get_regions(
             sample2path[samp], sample2p100s[samp], p100_to_p90, p100_to_p30, dbpath
