b46276568aed4f4f9b138982cbf36ff6a2dd6503
Matt Durrant <durrantmm@gmail.com>
2022-01-12 14:03:37 -0700

    update

--- GGDBFetch/ggdbfetch/clusters.py
+++ GGDBFetch/ggdbfetch/clusters.py
@@ -6,7 +6,7 @@ def p30_to_p100(p30_id, dbpath):
 
     con = sqlite3.connect(join(dbpath, CLUSTERS_DB))
     cur = con.cursor()
-    cmd = 'SELECT p100 FROM clusters WHERE p30_id="{}"'.format(p30_id)
+    cmd = 'SELECT p100 FROM clusters WHERE p30="{}"'.format(p30_id)
 
     p100s = set()
     for res in cur.execute(cmd):
