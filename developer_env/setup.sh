d0ccddbfc5cf16c7651ba92282ba90e0116ac9ff
JohnMcSpedon <john.p.mcspedon@gmail.com>
2022-02-11 09:52:11 +0000

    include neo4j in setup.sh

--- developer_env/setup.sh
+++ developer_env/setup.sh
@@ -19,6 +19,10 @@ DOCKER_BASH_HISTORY="$GENEGRAPHDB_REPO_DIR/data/docker.bash_history"
 touch $DOCKER_BASH_HISTORY
 
 DOCKER_IMAGE="genegraphdb"
+NEO4J_NETWORK_NAME="neo4j_network"
+NEO4J_DIR="$GENEGRAPHDB_REPO_DIR/data/neo4j"
+
+docker network create $NEO4J_NETWORK_NAME
 
 # docker aliases
 alias ggdb_docker_build="docker build -t $DOCKER_IMAGE $GENEGRAPHDB_REPO_DIR/developer_env"
@@ -29,6 +33,7 @@ alias ggdb_docker_run="docker run -it --rm \
     -v $GENEGRAPHDB_REPO_DIR:/GeneGraphDB \
     -v $HOME/.config/gcloud:/root/.config/gcloud \
     -v $GENEGRAPHDB_REPO_DIR/data/docker.bash_history:/root/.bash_history \
+    --network $NEO4J_NETWORK_NAME \
     $DOCKER_IMAGE"
     # -v /home/jupyter/GeneGraphDB/genegraph.db:/GeneGraphDB/data/genegraph.db \
 
@@ -38,6 +43,7 @@ alias ggdb_docker_jupyter="docker run -it --rm \
     -v $HOME/.config/gcloud:/root/.config/gcloud \
     -v $GENEGRAPHDB_REPO_DIR/data/docker.bash_history:/root/.bash_history \
     -p 0.0.0.0:8888:8888 \
+    --network $NEO4J_NETWORK_NAME \
     $DOCKER_IMAGE \
     jupyter notebook \
         --port=8888 \
@@ -45,3 +51,17 @@ alias ggdb_docker_jupyter="docker run -it --rm \
         --allow-root \
         --no-browser \
         --NotebookApp.custom_display_url=http://localhost:8888"
+
+# note the NEO4J_AUTH command determines
+# user ("neo4j"; cannot be changed)
+# password ("test"; can be changed)
+alias ggdb_neo4j_run="docker run -d --rm \
+    --name testneo4j \
+    -p7474:7474 -p7687:7687 \
+    -v $NEO4J_DIR/data:/data \
+    -v $NEO4J_DIR/logs:/logs \
+    -v $NEO4J_DIR/import:/var/lib/neo4j/import \
+    -v $NEO4J_DIR/plugins:/plugins \
+    --env NEO4J_AUTH=neo4j/test \
+    --network $NEO4J_NETWORK_NAME \
+    neo4j:latest"
