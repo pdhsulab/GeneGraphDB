87163c5eb65c4b55954a1d290b4b741e0478375c
JohnMcSpedon <john.p.mcspedon@gmail.com>
2022-04-25 22:20:57 -0600

    new GCS bucket

---
+++ prot_db/constants.py
@@ -0,0 +1,11 @@
+import datetime
+
+GCS_BUCKET_NAME = "gs://durrant_prot_pred_db"
+LOCAL_DATA_DIR = "/GeneGraphDB/data/durrant_prot_pred_db"
+
+TIMESTAMP_FORMAT = "%Y%m%dT%H:%M:%S"
+
+def get_timestamp(dt: datetime.datetime = None):
+    if dt is None:
+        dt = datetime.datetime.utcnow()
+    return dt.utcnow().strftime(TIMESTAMP_FORMAT)
