cb132fcb14969dfa997bce9d06c5ed72e38d2962
JohnMcSpedon <john.p.mcspedon@gmail.com>
2022-05-17 15:20:15 -0400

    local bigtable starts up

--- prot_db/bigtable_constants.py
+++ prot_db/bigtable_constants.py
@@ -2,34 +2,32 @@ import ctypes
 import os
 from typing import List, Tuple, Union
 
+import mmh3
+import numpy as np
 from Bio import SeqIO
 from google.cloud import bigtable
 from google.cloud.bigtable import column_family
 from google.cloud.environment_vars import BIGTABLE_EMULATOR
-import murmurhash
-import numpy as np
 
-from common import settings
-from common.util import mx_logging
+from prot_db import constants
+from utils import ggdb_logging
 
-# corresponds to instance name in ops/terraform/cambrium-protein-seq2func/bigtable.tf
-INSTANCE_ID = "mgnify-instance-200709"
-TABLE_ID = "merantix_seq2func"
+# TODO: define infrastructure in terraform including a bigtable.tf file
+INSTANCE_ID = "protein-pred-db"  # in durrant GCP project
+TABLE_ID = "protein-pred-v20220517"
 
 # Define column family ids and column IDs.  keep the strings short to save space in the table.
 # columns relating to raw sequence
 CF_ID_SEQUENCES = "seq"
 # raw amino acid sequence
-CID_RAW_AA_SEQ = "raa"
+COL_ID_RAW_AA_SEQ = "aa"
 # column relating to MGnify study as a source of data
-CF_ID_MGNIFY_STUDY = "mst"
-# columns relating to deepec predictions
-CF_ID_DEEPEC = "dec"
+CF_ID_MGNIFY_STUDY = "mgn_study"
 # garbage collection schema
 COLUMN_FAMILIES = {
     CF_ID_SEQUENCES: column_family.MaxVersionsGCRule(1),
+    # TODO: does this get rid of mgnify analyses?
     CF_ID_MGNIFY_STUDY: column_family.MaxVersionsGCRule(1),
-    CF_ID_DEEPEC: column_family.MaxVersionsGCRule(1),
 }
 
 # define a hash(sequence) -> 64 byte row key
@@ -55,9 +53,10 @@ def decode_to_float_tuple(encoded: bytes):
 
 def row_key(seq: Union[str, SeqIO.SeqRecord]):
     """Given an amino acid (AA) sequence, computes a unique 64 byte rowkey for use in bigtable."""
-    # TODO: check hash computed is same on 32 and 64-bit systems.
+    # TODO: check hash computed is same on 32 and 64-bit systems. I don't think this is the case for murmurhash
     if isinstance(seq, SeqIO.SeqRecord):
         seq = str(seq.seq)
+    assert isinstance(seq, str)
 
     prefix = seq[:_PREFIX_LEN]
     # unlikely, but pad prefix if necessary
@@ -68,7 +67,8 @@ def row_key(seq: Union[str, SeqIO.SeqRecord]):
     if len(suffix) < _SUFFIX_LEN:
         suffix = "_" * (_SUFFIX_LEN - len(suffix)) + suffix
 
-    hashed = murmurhash.mrmr.hash_unicode(seq, seed=_HASH_SEED)
+    # hashed = murmurhash.mrmr.hash_unicode(seq, seed=_HASH_SEED)
+    hashed = mmh3.hash(seq, seed=_HASH_SEED)  # seq.encode("utf-8")  # hash_unicode?
     # python returns hashes as a potentially unsigned ints.  this forces them to positive value
     # https://stackoverflow.com/a/18766695/3893740
     hashed = ctypes.c_uint32(hashed).value
@@ -81,7 +81,7 @@ def row_key(seq: Union[str, SeqIO.SeqRecord]):
 
 
 def get_row_key_boundary(boundary_idx, num_bits):
-    """ See Row Key Scans in the cambrium.bigtable README"""
+    """See Row Key Scans in prot_db/README.md"""
     assert num_bits <= 32, f"row_key hash is only 32 bits long. rows aren't uniformly random for {num_bits} bits"
     assert boundary_idx <= 2 ** num_bits, f"index {boundary_idx} is undefined for {num_bits} bits ({2 ** num_bits} max)"
     if boundary_idx == 2 ** num_bits:
@@ -98,7 +98,7 @@ def get_boundaries(num_bits):
 
 
 def get_seq_from_row(row):
-    return row.cells[CF_ID_SEQUENCES][CID_RAW_AA_SEQ.encode("utf-8")][0].value.decode()
+    return row.cells[CF_ID_SEQUENCES][COL_ID_RAW_AA_SEQ.encode("utf-8")][0].value.decode()
 
 
 def get_mgnify_study_to_analysis(row):
@@ -113,28 +113,28 @@ def get_mgnify_study_to_analysis(row):
 
 def get_local_client():
     # TODO: check process is running on 8086
-    mx_logging.warning("Using local bigtable emulator")
-    # Assumes bigtable container (and this container) is running on merantix_net.
-    # DNS name defined via --name var in bigtable_emulator.sh
+    ggdb_logging.warning("Using local bigtable emulator")
+    # Assumes bigtable container (and this container) is running on a shared docker network.
+    # DNS name defined via --name var in prot_db/bigtable_emulator.sh
     os.environ[BIGTABLE_EMULATOR] = "local_bigtable:8086"
     client = bigtable.Client(admin=True, project="sweet-sweet-emulation")
     return client
 
 
 def get_cloud_client():
-    mx_logging.info("Attempting to connect to cloud bigtable isntance")
-    client = bigtable.Client(admin=True, project=settings.GCP_PROJECT_BIO_SEQ2FUNC)
+    ggdb_logging.info("Attempting to connect to cloud bigtable isntance")
+    client = bigtable.Client(admin=True, project=constants.GCP_PROJECT_ID)
     return client
 
 
 def maybe_create_table(instance):
     table = instance.table(TABLE_ID)
     if table.exists():
-        mx_logging.info(f"Big Table table {TABLE_ID} already exists")
+        ggdb_logging.info(f"Big Table table {TABLE_ID} already exists")
         assert set(table.list_column_families().keys()) == set(COLUMN_FAMILIES.keys())
         # TODO: check that garbage collection policies are what we expect?, etc
     else:
-        mx_logging.info(f"Creating BigTable table with following column families: {COLUMN_FAMILIES}")
+        ggdb_logging.info(f"Creating BigTable table with following column families: {COLUMN_FAMILIES}")
         table.create(column_families=COLUMN_FAMILIES)
     return table
 
--- prot_db/bigtable_emulator.sh
+++ prot_db/bigtable_emulator.sh
@@ -1,1 +1,5 @@
-docker run --rm -it -p 8086:8086 --net merantixnet --name local_bigtable  bigtruedata/gcloud-bigtable-emulator start --host-port=0.0.0.0:8086
+docker run \
+    --rm -it \
+    -p 8086:8086 --net neo4j_network \
+    --name local_bigtable  \
+    bigtruedata/gcloud-bigtable-emulator start --host-port=0.0.0.0:8086
