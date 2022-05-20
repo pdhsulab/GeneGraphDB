40c55b62842ceb7f2ea02ddc3a1889d7c6f36eb0
JohnMcSpedon <john.p.mcspedon@gmail.com>
2022-05-20 16:04:36 -0400

    parse GFF file

--- utils/fasta_util.py
+++ utils/fasta_util.py
@@ -1,10 +1,28 @@
 import gzip
-from typing import Iterator
+import os
+import subprocess
+from dataclasses import dataclass
+from typing import Iterator, List
 
 from Bio import SeqIO
 
+from utils import file_util
+
 GZIP_SUFFIXES = [".gz", ".gzip"]
 FASTA_SUFFIXES = [".fasta", ".fna", ".ffn", ".faa", ".frn"]
+
+
+@dataclass
+class GffRow:
+    seqid: str
+    source: str
+    type_: str
+    start: str
+    end: str
+    score: str
+    strand: str
+    phase: str
+    attributes: str
 
 
 # TODO: support cloud files automatically?
@@ -29,3 +47,24 @@ def open_fasta(fpath) -> Iterator[SeqIO.SeqRecord]:
     else:
         for record in SeqIO.parse(fpath, "fasta"):
             yield record
+
+
+def parse_gff_file(gff_file) -> List[GffRow]:
+    "Parse GFF file into fields from https://en.wikipedia.org/wiki/General_feature_format"
+    annotations_fname = os.path.basename(gff_file)
+
+    with file_util.tmp_copy_on_open(gff_file, annotations_fname) as local_file:
+        if local_file.endswith(".bgz"):
+            # replace suffix so `gunzip` utility works
+            gzip_name = local_file[: -len(".bgz")] + ".gz"
+            cmd = f"mv {local_file} {gzip_name} && gunzip {gzip_name}"
+            _ = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            local_file = gzip_name[: -len(".gz")]
+
+        with open(local_file) as fh:
+            gff_lines = [line.rstrip() for line in fh.readlines()]
+        gff_lines = [line.rstrip() for line in gff_lines]
+        assert gff_lines[0] == "##gff-version 3"
+        gff_lines = gff_lines[1:]
+        gff_rows = [GffRow(*line.split("\t")) for line in gff_lines]
+    return gff_rows
